86b854643c6a44971fd436aaa323495bec65c78a
“DavideMira” <84937644+DavideMira@users.noreply.github.com>
2022-01-04 17:06:09 +0100

    modify

--- ProjectEsame/dungeon.lua
+++ ProjectEsame/dungeon.lua
@@ -24,12 +24,42 @@ arrowLeft.y = display.contentCenterY
 arrowLeft.name = "down"
 
 local button = display.newImageRect(control, "/risorseGrafiche/risorseTmp_perTest/arrows/redButton.png",80,80)
-button.x =display.contentCenterX
-button.y = display.contentCenterX
+button.x = display.contentCenterX
+button.y = display.contentCenterY
 button.name = "button"
 
+local pg = display.newImageRect(camera, "/risorseGrafiche/risorseTmp_perTest/alienYellow.png", 124,108 )
+pg.x= display.contentCenterX
+pg.y= display.contentCenterY
+pg.name= "pg"
+
+local function movePg(event)
+	local arrow=event.target
+	
+	if event.phase == "began" then
+	
+		if arrow.name == "left" then
+			pg:setLinearVelocity(-200, 0)
+	    	 
+		elseif arrow.name == "right" then
+        	pg:setLinearVelocity(200,0)
+
+        elseif arrow.name == "up" then
+        	pg:setLinearVelocity(0,-200)
+            
+        elseif arrow.name == "down" then
+        	pg:setLinearVelocity(0,200)    
+	   end
+	
+	elseif event.phase == "ended" then
+			pg:setLinearVelocity(0,0)
+	end
+ 	   	 
+ 	return true
+end
+
 -- add event to arrows and button
---arrowLeft:addEventListener("touch", movePg)
---arrowRight:addEventListener("touch", movePg)
---arrowDown:addEventListener("touch", movePg)
---arrowUp:addEventListener("touch", movePg)
+arrowLeft:addEventListener("touch", movePg)
+arrowRight:addEventListener("touch", movePg)
+arrowDown:addEventListener("touch", movePg)
+arrowUp:addEventListener("touch", movePg)
