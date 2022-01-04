2635544056ff536205adf4ca6d19adeb404fbf58
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-01-04 16:09:48 +0100

    aggiunto e sistemato la roba

--- ProjectEsame/platformVerticale.lua
+++ ProjectEsame/platformVerticale.lua
@@ -1,15 +1,28 @@
 function createButtons() -- crea le frecce utili per sx/dx/jmp 
 						-- non local in modo tale che i suoi valori possano essere 
-	local control = display.newGroup() --crea il gruppo control, dove mette tutta la roba 
-
-	local arrowUp = display.newImageRect(control, "\\risorseGrafiche\\risorseTmp_perTest\\arrows\arrowUp.png", 80, 80)
+	-- local control = display.newGroup() --crea il gruppo control, dove mette tutta la roba 
+	control = {}
+	local allArrows = display.newGroup()
+	local arrowUp = display.newImageRect(allArrows, "/risorseGrafiche/risorseTmp_perTest/arrows/arrowUp.png", 100, 100)
 	arrowUp.name = "up"
+	arrowUp.x = 500
+	arrowUp.y = 525
 
-	local arrowLeft = display.newImageRect(control, "\\risorseGrafiche\\risorseTmp_perTest\\arrows\arrowLeft.png", 80, 80)
+	local arrowLeft = display.newImageRect(allArrows, "/risorseGrafiche/risorseTmp_perTest/arrows/arrowLeft.png",100, 100)
 	arrowLeft.name = "left"
+	arrowLeft.x = 425
+	arrowLeft.y = 600
 
-	local arrowRight = display.newImageRect(control, "\\risorseGrafiche\\risorseTmp_perTest\\arrows\arrowRight.png", 80, 80)
+	local arrowRight = display.newImageRect(allArrows, "/risorseGrafiche/risorseTmp_perTest/arrows/arrowRight.png", 100, 100)
 	arrowRight.name = "right"
+	arrowRight.x = 575
+	arrowRight.y = 600
+	
+	table.insert(control, allArrows)
+	table.insert(control, arrowUp)
+	table.insert(control, arrowLeft)
+	table.insert(control, arrowRight)
+	
 
 	return control
 end
