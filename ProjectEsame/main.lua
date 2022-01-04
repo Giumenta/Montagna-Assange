13ca5eac42445251307653af451c84aface16c75
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-01-04 12:48:58 +0100

    aggiunto segmento per link a platformVerticale.lua

--- ProjectEsame/main.lua
+++ ProjectEsame/main.lua
@@ -5,3 +5,19 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+--//yooo
+
+-- PARTE DI CODICE PER IL PLATFORM VERTICALE (3 LIVELLO)
+-- fromVeeko says: non so di preciso come fare a testare la roba, quindi linko qua il file che sto iniziando a
+-- fare e vedo di farlo funzionare. Commento il resto del codice e prima di finire dovrei ricordarmi sempre di 
+-- scommentarlo, però ehi non escludo di essere studpido e dimenticarmi di farlo quindi cheers non prendetevela	
+
+require "platformVerticale" --dovrebbe richiamare il file creato
+
+local createdButtons
+createdButtons = createButtons() --richiamo la funzione presente in platformVerticale, mezzo test che funga tutto
+print(createdButtons)
+
+
+--FINE PARTE PROVA CODICE PLATFORM VERTICALE (3 LIVELLO)
