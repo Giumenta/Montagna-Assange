01516708ec2a0a854c5aa5a0c6f723b2c3955160
“DavideMira” <84937644+DavideMira@users.noreply.github.com>
2022-01-04 17:41:13 +0100

    Modify moved pg

--- ProjectEsame/dungeon.lua
+++ ProjectEsame/dungeon.lua
@@ -1,3 +1,7 @@
+physics = require("physics")
+physics.start()
+physics.setGravity(0,0)
+
 -- create group for non fixed obj (camera) and for fixed obj(control)
 local camera= display.newGroup()
 local control = display.newGroup()
@@ -5,45 +9,58 @@ local control = display.newGroup()
 -- create obj arrows and button for interaction
 local arrowLeft = display.newImageRect(control,"/risorseGrafiche/risorseTmp_perTest/arrows/arrowLeft.png",80,80)
 arrowLeft.x = 100
-arrowLeft.y = display.contentCenterY
+arrowLeft.y = display.contentHeight-150
 arrowLeft.name = "left"
 
 local arrowRight = display.newImageRect(control,"/risorseGrafiche/risorseTmp_perTest/arrows/arrowRight.png",80,80)
-arrowRight.x = display.contentWidth-100
-arrowRight.y = display.contentCenterY
+arrowRight.x = 260
+arrowRight.y = display.contentHeight-150
 arrowRight.name = "right"
 
 local arrowUp = display.newImageRect(control,"/risorseGrafiche/risorseTmp_perTest/arrows/arrowUp.png",80,80)
-arrowLeft.x = 100
-arrowLeft.y = display.contentCenterY
-arrowLeft.name = "up"
+arrowUp.x = 180
+arrowUp.y = display.contentHeight-200
+arrowUp.name = "up"
 
 local arrowDown = display.newImageRect(control,"/risorseGrafiche/risorseTmp_perTest/arrows/arrowDown.png",80,80)
-arrowLeft.x = 100
-arrowLeft.y = display.contentCenterY
-arrowLeft.name = "down"
+arrowDown.x = 180
+arrowDown.y = display.contentHeight-100
+arrowDown.name = "down"
 
-local button = display.newImageRect(control, "/risorseGrafiche/risorseTmp_perTest/arrows/redButton.png",80,80)
-button.x = display.contentCenterX
-button.y = display.contentCenterY
-button.name = "button"
+--local button = display.newImageRect(control, "/risorseGrafiche/risorseTmp_perTest/arrows/redButton.png",80,80)
+--button.x = display.contentCenterX
+--button.y = display.contentCenterY
+--button.name = "button"
 
 local pg = display.newImageRect(camera, "/risorseGrafiche/risorseTmp_perTest/alienYellow.png", 124,108 )
 pg.x= display.contentCenterX
 pg.y= display.contentCenterY
 pg.name= "pg"
+physics.addBody(pg, "dynamic")
 
 local function movePg(event)
 	local arrow=event.target
 	
 	if event.phase == "began" then
-	
+        if arrow.name == "left" then
+			pg:setLinearVelocity(-200, 0)
+	    	 
+		elseif arrow.name == "right" then
+        	pg:setLinearVelocity(200,0)
+            
+        elseif arrow.name == "up" then
+        	pg:setLinearVelocity(0,-200)
+            
+        elseif arrow.name == "down" then
+        	pg:setLinearVelocity(0,200)    
+	   end
+    elseif event.phase == "moved" then
 		if arrow.name == "left" then
 			pg:setLinearVelocity(-200, 0)
 	    	 
 		elseif arrow.name == "right" then
         	pg:setLinearVelocity(200,0)
-
+            
         elseif arrow.name == "up" then
         	pg:setLinearVelocity(0,-200)
             
