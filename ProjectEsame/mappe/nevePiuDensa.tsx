a1bd29c0c0c4d2eacb185ed7e1336cd8619ae145
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-01-05 14:58:50 +0100

    caos on tiled

---
+++ ProjectEsame/mappe/nevePiuDensa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="nevePiuDensa" tilewidth="32" tileheight="32" tilecount="255" columns="17">
+ <image source="../risorseGrafiche/risorseTmp_perTest/ipotesiPlatform/ip0/SET1_Mainlev_build_CON_NEVE.png" width="544" height="480"/>
+</tileset>
