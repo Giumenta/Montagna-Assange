0c26689967f1626f8a13ac70313ad7cbe80defaf
“DavideMira” <84937644+DavideMira@users.noreply.github.com>
2022-01-27 16:11:48 +0100

    sync

--- ProjectEsame/dungeon.lua
+++ ProjectEsame/dungeon.lua
@@ -1,25 +1,26 @@
 physics = require("physics")
 physics.start()
 physics.setGravity(0,0)
---physics.setDrawMode("hybrid")
+physics.setDrawMode("hybrid")
 
 local tiled = require "com.ponywolf.ponytiled"
 local json = require ("json")
 local mapData = json.decodeFile(system.pathForFile("Maptiles/Map1.json",system.ResourceDirectory))
 local map = tiled.new(mapData, "Maptiles")
 
-map:scale(3.5,3.5)
-map.x =-150
-map.y=-100
-
-local mapBorderLeft = 0
-local mapBorderRight = 1280
-local mapBorderTop = 0
-local mapBorderBottom = 720
-
 -- create group for non fixed obj (camera) and for fixed obj(control)
 local camera= display.newGroup()
 local control = display.newGroup()
+
+map:scale(3.5,3.5)
+map.x =-150
+map.y=-100
+camera:insert(map)
+
+--local mapBorderLeft = 0
+--local mapBorderRight = 1280
+--local mapBorderTop = 0
+--local mapBorderBottom = 720
 
 -- create obj arrows and button for interaction
 local arrowLeft = display.newImageRect(control,"/risorseGrafiche/risorseTmp_perTest/arrows/arrowLeft.png",80,80)
@@ -51,6 +52,8 @@ arrowDown.name = "down"
 local hero = map:findObject("pg")
 hero:toFront()
 hero.isFixedRotation=true
+print(hero.x)
+print(hero.y)
 --hero.x= display.contentCenterX
 --hero.y= display.contentCenterY
 --hero.name= "hero"
@@ -93,38 +96,7 @@ local function movePg(event)
  	return true
 end
 ------- FUNZIONE PER MOVIMENTO CAMERA DA METTERE A POSTO -------
-local function moveCamera(event)
-	local offsetX = hero.width*2
-	local offsetY = hero.height*2
-	
-	local displayLeft = -camera.x
-	local displayTop = -camera.y
-	
-	local nonScrollingWidth =  display.contentWidth-offsetX
-	local nonScrollingHeight = display.contentHeight-offsetY
-	
-	
-	if hero.x >= mapBorderLeft+offsetX 
-	   and hero.x <= mapBorderRight - offsetX then
-		  
-		  if hero.x>displayLeft+nonScrollingWidth then
-	        	    camera.x = -hero.x + nonScrollingWidth
-	      elseif hero.x < displayLeft+offsetX then
-	            	camera.x = -hero.x + offsetX	
-	      end
-	end
-    
- 	if hero.y >= mapBorderTop+offsetY then
-	    
-	    if hero.y>displayTop+nonScrollingHeight then
-		    camera.y = -hero.y + nonScrollingHeight
-		elseif hero.y < displayTop+offsetY then
-		  camera.y = -hero.y + offsetY	
-	    end	 
-	end	 
-		
-	return true	
-end
+
 
 
 -- add event to arrows and button
@@ -136,5 +108,5 @@ arrowUp:addEventListener("touch", movePg)
 Runtime:addEventListener("enterFrame",moveCamera)
 
 
-local dragable = require "com.ponywolf.plugins.dragable"
-map = dragable.new(map)
+--local dragable = require "com.ponywolf.plugins.dragable"
+--map = dragable.new(map)
