16975476a93cd89d43f6fce5ea54ff5d67a30eb2
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-03-02 10:12:42 +0100

    solito trantran

--- ProjectEsame/platformVerticale.lua
+++ ProjectEsame/platformVerticale.lua
@@ -11,23 +11,50 @@ dispWidth = display.contentWidth
 dispHeight = display.contentHeight
 --aggancio il fondo mappa
 --428*16 = 6848
+local mapHeight = 6848
 map.y = -6848 + dispHeight
 
 --creo i gruppi
 local camera = display.newGroup() --gruppo dove metto dentro tutta la roba che debe essere sempre visibile
 local control = display.newGroup() --gruppo per le freccette
 --carico le risorse grafiche
-local opt = {width = 32, height = 32, numFrames = 3}
-local hero = graphics.newImageSheet("risorseGrafiche/PG/sprite-sheet.png", opt)
-local seqs = {
+local opt = { width = 32, height = 32, numFrames = 12}
+local heroSheet = graphics.newImageSheet("risorseGrafiche/PG/sprite-sheet.png",opt)
+
+   local heroSeqs ={
 	{
-		name = "static" 
-		start = 1 --ma inizia con indice 0 o 1?
-		count 
-	}
+		name = "Front",
+		frames={1,2,3},
+		time = 1000,
+		loopCount = 0,
+		loopDirection ="forward"
+   	},
+   	{
+		name = "Left",
+		frames={4,5,6},
+		time = 1000,
+		loopCount = 0,
+		loopDirection ="forward"
+   	},
 	{
-		
-		name = "runLeft",
-		start = 1
+		name = "Right",
+		frames={7,8,9},
+		time = 1000,
+		loopCount = 0,
+		loopDirection ="forward"
+	},
+	{
+		name = "Back",
+		frames={10,11,12},
+		time = 1000,
+		loopCount = 0,
+		loopDirection ="forward"
 	}
 }
+local hero = display.newSprite(heroSheet,heroSeqs)
+local heroShape={-6,0,6,0,-6,16,6,16}
+physics.addBody(hero, "dynamic", heroShape)
+hero:scale(2,2)
+hero.isFixedRotation=true
+hero.x = (display.dispWidth) / 2
+hero.y = -mapHeight + 200
