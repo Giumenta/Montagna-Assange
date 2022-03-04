0999cde57ce5899aa7dc6c22f2b6b4427741dd93
Marinski99 <riccardomarin99@gmail.com>
2022-03-04 17:40:11 +0100

    inizio 15game

--- ProjectEsame/15game.lua
+++ ProjectEsame/15game.lua
@@ -1,3 +1,27 @@
-local tassello
-local griglia= display.newRect(display.contentCenterX,display.contentCenterY, display.contentHeight*0.9,display.contentHeight*0.9)
-print(display.contentHeight)
+
+
+local function creaGriglia() 
+    local tasselli={}--dichiaro che tasselli è una tabella
+    local larghezzaGriglia = display.contentHeight*0.9
+    local griglia= display.newRect(display.contentCenterX,display.contentCenterY, larghezzaGriglia,larghezzaGriglia)
+    local dimtassello= (display.contentHeight*0.88)/4
+    local colonna=1
+    local riga=1
+    local spaziaturaTasselli=(larghezzaGriglia-display.contentHeight*0.88)/5
+    for riga=1, 4 do
+        for colonna=1,4 do
+        local tassello = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassellogioco15.jpg",dimtassello, dimtassello)
+        tassello.anchorX=0
+        tassello.anchorY=0
+        tassello.y=display.contentHeight/2-(larghezzaGriglia)/2 + (riga-1)*dimtassello + riga*spaziaturaTasselli
+        tassello.x=display.contentWidth/2-(larghezzaGriglia)/2 + (colonna-1)*dimtassello + colonna*spaziaturaTasselli
+        end
+    end
+end
+
+creaGriglia()
+--legare i numeri ai tasselli
+--randomizzare i numeri sui tasselli
+--eliminare il 16° tassello
+--rendere spostabili i tasselli
+--aggiungere sfondo
