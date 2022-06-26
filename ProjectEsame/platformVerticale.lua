09dd17a52a031c367d137068646c9b9615985739
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-06-26 18:55:52 +0200

    giusto due sistemazioni
    Co-authored-by: DavideMira <DavideMira@users.noreply.github.com>

--- ProjectEsame/platformVerticale.lua
+++ ProjectEsame/platformVerticale.lua
@@ -61,6 +61,27 @@ local heroSeqs ={
 	}
 }
 
+--preparazione frecce
+local arrowLeft = display.newImageRect(control,"risorseGrafiche/risorseTmp_perTest/arrows/arrowLeft.png",80,80)
+arrowLeft.x = 100
+arrowLeft.y = display.contentHeight-150
+arrowLeft.name = "left"
+
+local arrowRight = display.newImageRect(control,"risorseGrafiche/risorseTmp_perTest/arrows/arrowRight.png",80,80)
+arrowRight.x = 260
+arrowRight.y = display.contentHeight-150
+arrowRight.name = "right"
+
+local arrowUp = display.newImageRect(control,"risorseGrafiche/risorseTmp_perTest/arrows/arrowUp.png",80,80)
+arrowUp.x = 180
+arrowUp.y = display.contentHeight-200
+arrowUp.name = "up"
+
+local arrowDown = display.newImageRect(control,"risorseGrafiche/risorseTmp_perTest/arrows/arrowDown.png",80,80)
+arrowDown.x = 180
+arrowDown.y = display.contentHeight-100
+arrowDown.name = "down"
+
 local hero = display.newSprite(heroSheet,heroSeqs)
 local heroShape={-6,0,6,0,-6,16,6,16}
 physics.addBody(hero, "dynamic", heroShape)
@@ -73,5 +94,51 @@ if hero.y < display.contentHeight*2 then
 	hero.y = 0
 end
 
-	print(hero.x)
-	print(hero.y)
+print(hero.x)
+print(hero.y)
+
+local function movePg(event)
+	local arrow = event.target
+	local ciao = 'ciao'
+
+	if event.phase == "began" then
+		if arrow.name == "up" then
+			hero:setSequence("back")
+			hero:play()
+			hero:setLinearVelocity(0, -150)			
+		elseif arrow.name == "left" then
+			hero:setSequence("left")
+			hero:play()
+			hero:setLinearVelocity(-150, 0)
+		elseif arrow.name == "right" then
+			hero:setSequence("right") 
+			hero:play()
+			hero:setLinearVelocity(150, 0)
+		else --arrow.name == down
+			hero:setSequence("front")
+			hero:play()
+			hero:setLinearVelocity(0, 150)
+		end
+	elseif event.phase == "moved" then
+		if arrow.name == "up" then
+			hero:setLinearVelocity(0, -150)
+		elseif arrow.name == "left" then
+			hero:setLinearVelocity(-150, 0)
+		elseif arrow.name == "right" then 
+			hero:setLinearVelocity(150, 0)
+		else --arrow.name == down
+			hero:setLinearVelocity(0, 150)
+		end
+	elseif event.phase == "ended" then
+		hero:setLinearVelocity(0,0)
+		hero:pause()
+	end
+
+	return true
+end
+
+
+arrowLeft:addEventListener("touch", movePg)
+arrowRight:addEventListener("touch", movePg)
+arrowDown:addEventListener("touch", movePg)
+arrowUp:addEventListener("touch", movePg)
--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 L’idea a cui avevamo pensato è la seguente:
 Il nostro pg parte dalla base di una montagna e tramite dei livelli , ognuno caratterizzato da un genere a sé, deve raggiungerne la cima.
 ---
-__Parte 1__ : Schermata a scorrimento orizzontale con presentazione personaggio e ingresso in una grotta (primo livello) (Ingresso primo livello tramite rompicapo?-> gioco del 15?)<br>
-__Primo Livello__ : Platform a scorrimento orizzontale<br>---<br>
-__Parte 2__ : Uscita dal primo livello e Platform a scorrimento verticale (con bioma diverso dalla base della montagna). <br>
-__Secondo Livello__ : Livello Dungeon (con nemici e oggetti da gestire??)Uscita dal secondo livello (tramite secondo rompicapo?-> gioco formine per bambini?)<br>---<br>
+__Parte 1__ : Schermata a scorrimento orizzontale con presentazione personaggio<br>
+__Primo Livello__ : Platform a scorrimento verticale<br>---<br>
+__Parte 2__ : <br>
+__Secondo livello__: gioco del 15 per entrare il dungeon<br>
+__Terzo Livello__ : Livello Dungeon (con nemici e oggetti da gestire??)Uscita dal secondo livello (con chiavi da raccogliere per uscire)<br>---<br>
 __Parte 3__ : Uscita secondo livello e secondo platform verticale con bioma diverso e meccaniche diverse (aggiunta attrito diverso dovuto a neve/ghiaccio, strumenti da usare?) fino a raggiungere cima della montagna.<br>
 Scena finale in cima alla montagna con alcune linee di dialogo<br>---<br>
 __Livello extra__ : Discesa dalla montagna <br> --- <br><br>
