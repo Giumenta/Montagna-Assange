1ef71ace9263199a7d2e87dbd71e2cccb63a0e00
Marinski99 <riccardomarin99@gmail.com>
2022-06-28 15:52:48 +0200

    numeri

--- ProjectEsame/15game.lua
+++ ProjectEsame/15game.lua
@@ -10,14 +10,29 @@ local function creaGriglia()
     local spaziaturaTasselli=(larghezzaGriglia-display.contentHeight*0.88)/5
     for riga=1, 4 do
         for colonna=1,4 do
-        local tassello = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassellogioco15.jpg",dimtassello, dimtassello)
+        local tassello = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassellogioco15_stondato.jpg",dimtassello, dimtassello)
         tassello.anchorX=0
         tassello.anchorY=0
         tassello.y=display.contentHeight/2-(larghezzaGriglia)/2 + (riga-1)*dimtassello + riga*spaziaturaTasselli
         tassello.x=display.contentWidth/2-(larghezzaGriglia)/2 + (colonna-1)*dimtassello + colonna*spaziaturaTasselli
-        end
+        
+		
+			local numeri = display.newText (
+               ((colonna - 1) * 4) + riga,
+                (riga - 1) * dimtassello,
+                (colonna - 1) * dimtassello, native.systemFont, 55
+            )
+			--myText:setFillColor( 1, 0, 0 )
+		numeri.anchorX=0
+        numeri.anchorY=0
+		numeri.y=display.contentHeight/2-(larghezzaGriglia)/2 + (riga-1)*dimtassello + riga*spaziaturaTasselli
+        numeri.x=display.contentWidth/2-(larghezzaGriglia)/2 + (colonna-1)*dimtassello + colonna*spaziaturaTasselli
+		
+		end
+		
     end
 end
+
 
 creaGriglia()
 --legare i numeri ai tasselli
