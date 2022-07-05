97ef6644ef09956e7cb7ff521118fa88d73ff285
Marinski99 <riccardomarin99@gmail.com>
2022-07-05 12:37:04 +0200

    test transition

--- ProjectEsame/gioco15.lua
+++ ProjectEsame/gioco15.lua
@@ -3,10 +3,10 @@ local riga=1
 local GRID_WIDTH = 4
 local GRID_HEIGHT = 4
 local grid = {} -- creo una griglia 4x4
-		for  colonna = 1, GRID_HEIGHT do
-			grid[colonna] = {}
-				for riga = 1, GRID_WIDTH do
-				grid[colonna][riga] = ((colonna - 1) * GRID_WIDTH) + riga
+		for  riga = 1, GRID_HEIGHT do
+			grid[riga] = {}
+				for colonna = 1, GRID_WIDTH do
+				grid[riga][colonna] = ((colonna - 1) * GRID_WIDTH) + riga
 				end
 		end
 
@@ -24,25 +24,25 @@ local function creaGriglia()
 	local tassello
 	--assegno ad ogni posizione nella griglia un tassello specifico
 	grid[1][1] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello1.png",dimtassello, dimtassello )
-	grid[2][1] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello2.png",dimtassello, dimtassello )
-	grid[3][1] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello3.png",dimtassello, dimtassello )
-	grid[4][1] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello4.png",dimtassello, dimtassello )
-	grid[1][2] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello5.png",dimtassello, dimtassello )
+	grid[1][2] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello2.png",dimtassello, dimtassello )
+	grid[1][3] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello3.png",dimtassello, dimtassello )
+	grid[1][4] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello4.png",dimtassello, dimtassello )
+	grid[2][1] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello5.png",dimtassello, dimtassello )
 	grid[2][2] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello6.png",dimtassello, dimtassello )
-	grid[3][2] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello7.png",dimtassello, dimtassello )
-	grid[4][2] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello8.png",dimtassello, dimtassello )
-	grid[1][3] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello9.png",dimtassello, dimtassello )
-	grid[2][3] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello10.png",dimtassello, dimtassello )
+	grid[2][3] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello7.png",dimtassello, dimtassello )
+	grid[2][4] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello8.png",dimtassello, dimtassello )
+	grid[3][1] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello9.png",dimtassello, dimtassello )
+	grid[3][2] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello10.png",dimtassello, dimtassello )
 	grid[3][3] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello11.png",dimtassello, dimtassello )
-	grid[4][3] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello12.png",dimtassello, dimtassello )
-	grid[1][4] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello13.png",dimtassello, dimtassello )
-	grid[2][4] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello14.png",dimtassello, dimtassello )
-	grid[3][4] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello15.png",dimtassello, dimtassello )
+	grid[3][4] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello12.png",dimtassello, dimtassello )
+	grid[4][1] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello13.png",dimtassello, dimtassello )
+	grid[4][2] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello14.png",dimtassello, dimtassello )
+	grid[4][3] = display.newImageRect("risorseGrafiche/montagnaGenericoAmbiente/tassello15.png",dimtassello, dimtassello )
 	
 	for colonna=1,4 do        
         for riga=1,4 do
-			if grid[colonna][riga] ~= 4*4 then -- rimuovo 16° tassello
-				tassello = grid[colonna][riga]
+			if grid[riga][colonna] ~= 4*4 then -- rimuovo 16° tassello
+				tassello = grid[riga][colonna]
 				tassello.anchorX=0
 				tassello.anchorY=0
 				tassello.y=display.contentHeight/2-(larghezzaGriglia)/2 + (riga-1)*dimtassello + riga*spaziaturaTasselli
@@ -64,7 +64,7 @@ local function onKeyEvent (event)
 			for colonna=1, GRID_HEIGHT do
         
 				for riga=1, GRID_WIDTH do
-					if grid[colonna][riga] == GRID_WIDTH*GRID_HEIGHT  then
+					if grid[riga][colonna] == GRID_WIDTH*GRID_HEIGHT  then
 					emptyX = riga
 					emptyY = colonna
 					
@@ -87,8 +87,8 @@ local function onKeyEvent (event)
 			grid[newEmptyY][emptyX], grid[emptyY][emptyX] =
 			grid[emptyY][emptyX], grid[newEmptyY][emptyX]
 			--local tassello2 = grid[emptyY][emptyX]
-			local tassello = grid[newEmptyY][emptyX]
-			transition.moveTo(tassello, {x=100, y= 11, time=100})
+			--local tassello = grid[newEmptyY][emptyX]
+			transition.moveTo(grid[3][4], {x=100, y= 11, time=100})
 		end
 	--return false
 end
