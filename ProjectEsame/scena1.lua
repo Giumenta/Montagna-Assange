b0d677a5aa6043ba896964949031437b6674e643
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-07-14 18:39:25 +0200

    aggiunto sfondo
    Co-authored-by: Giumenta <Giumenta@users.noreply.github.com>

---
+++ ProjectEsame/scena1.lua
@@ -0,0 +1,109 @@
+-----------------------------------------------------------------------------------------
+--
+-- replay.lua   defines the  replay scene
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+ 
+local scene = composer.newScene()
+ 
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+ 
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+ 
+local background  -- variable that stores the background image 
+local retry       -- variable that stores the retry image button SERVE? 
+
+-- create()
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+	
+	-- Load the background image
+	background =
+	
+	
+	sceneGroup:insert(background)	  
+end
+ 
+ 
+-- restart is a table listener associated with the retry button
+-- which is executed when the retry button is tapped 
+local function restart()
+	-- go to the game scene
+	composer.removeScene("game")
+	composer.gotoScene("game")
+	return true
+end
+	 
+-- show()
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        
+		-- place the retry object at the center of the display
+		background.x = display.contentCenterX
+		background.y = display.contentCenterY
+		-- place the retry object at the center of the display
+		retry.x = display.contentCenterX
+		retry.y = display.contentCenterY
+		
+ 
+    elseif ( phase == "did" ) then
+        -- activate the tap listener 
+		retry.tap = restart
+		retry:addEventListener("tap", restart)
+    end
+end
+ 
+ 
+-- hide()
+function scene:hide( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
+		-- Remove the tap listener associated with the retry button
+		retry:removeEventListener("tap",replay)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
+ 
+    end
+end
+ 
+ 
+-- destroy()
+function scene:destroy( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+ 
+end
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+ 
+return scene
+
+	
