0d1f65fb7e8470578bc765fb2bae84f210d4c252
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-07-14 13:28:42 +0200

    audio da sistemare

--- ProjectEsame/dungeon.lua
+++ ProjectEsame/dungeon.lua
@@ -13,7 +13,7 @@ local dragable = require "com.ponywolf.plugins.dragable"
 map = dragable.new(map)
 
 --local heroLib=require("herolib")
-local passi = audio.loadStream("RisorseAudio/walkingdead.wav")
+local passi = audio.loadSound("RisorseAudio/walkingdeadmp3.mp3")
 
 -- create group for non fixed obj (camera) and for fixed obj(control)
 local camera= display.newGroup()
