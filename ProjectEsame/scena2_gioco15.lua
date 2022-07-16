28f815f7f2792ee217900e740d56cf80b00a63c7
“DavideMira” <84937644+DavideMira@users.noreply.github.com>
2022-07-16 12:41:04 +0200

    Push

--- ProjectEsame/scena2_gioco15.lua
+++ ProjectEsame/scena2_gioco15.lua
@@ -71,44 +71,44 @@ local function isRisolto()
 end
 
 
-local function shuffle()
-
-	local moveNumber
-		for moveNumber = 1, 1000 do
-			local emptyX
-			local emptyY
-			
-			for colonna = 1, GRID_HEIGHT do
-				for riga = 1, GRID_WIDTH do
-					if grid[riga][colonna] == nil then
-						emptyX = colonna
-						emptyY = riga
-					end
-				end
-			end
-			
-			local newEmptyY = emptyY
-			local newEmptyX = emptyX
-			
-			local roll = math.random(4)
-				
-			if roll == 1 then
-				newEmptyY = emptyY - 1
-			elseif roll == 2 then
-				newEmptyY = emptyY + 1
-			elseif roll == 3 then
-				newEmptyX = emptyX - 1
-			elseif roll == 4 then
-				newEmptyX = emptyX + 1
-			end
-			
-				if grid[newEmptyY] and grid[newEmptyY][newEmptyX] then
-					grid[newEmptyY][newEmptyX], grid[emptyY][emptyX] =
-					grid[emptyY][emptyX], grid[newEmptyY][newEmptyX]
-					transition.moveTo(grid[emptyY][emptyX], {x=display.contentWidth/2-(larghezzaGriglia)/2 + (emptyX-1)*dimtassello + emptyX*spaziaturaTasselli, y= display.contentHeight/2-(larghezzaGriglia)/2 + (emptyY-1)*dimtassello + emptyY*spaziaturaTasselli, time=10})
-				end
-		end
-end
+--local function shuffle()
+--
+--	local moveNumber
+--		for moveNumber = 1, 1000 do
+--			local emptyX
+--			local emptyY
+--			
+--			for colonna = 1, GRID_HEIGHT do
+--				for riga = 1, GRID_WIDTH do
+--					if grid[riga][colonna] == nil then
+--						emptyX = colonna
+--						emptyY = riga
+--					end
+--				end
+--			end
+--			
+--			local newEmptyY = emptyY
+--			local newEmptyX = emptyX
+--			
+--			local roll = math.random(4)
+--				
+--			if roll == 1 then
+--				newEmptyY = emptyY - 1
+--			elseif roll == 2 then
+--				newEmptyY = emptyY + 1
+--			elseif roll == 3 then
+--				newEmptyX = emptyX - 1
+--			elseif roll == 4 then
+--				newEmptyX = emptyX + 1
+--			end
+--			
+--				if grid[newEmptyY] and grid[newEmptyY][newEmptyX] then
+--					grid[newEmptyY][newEmptyX], grid[emptyY][emptyX] =
+--					grid[emptyY][emptyX], grid[newEmptyY][newEmptyX]
+--					transition.moveTo(grid[emptyY][emptyX], {x=display.contentWidth/2-(larghezzaGriglia)/2 + (emptyX-1)*dimtassello + emptyX*spaziaturaTasselli, y= display.contentHeight/2-(larghezzaGriglia)/2 + (emptyY-1)*dimtassello + emptyY*spaziaturaTasselli, time=10})
+--				end
+--		end
+--end
 
 --creo una funzione per spostare i tasselli nella casella libera
 local function muovitassello (self, event)
@@ -237,7 +237,7 @@ local function creaGriglia()
 			end
 		end
 	end
-	shuffle()--mescolo i tasselli
+	--shuffle()--mescolo i tasselli
 end
 
 function scene:create( event )
