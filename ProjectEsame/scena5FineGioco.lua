9b06f8b2542354e67241eee77f6aa7ea97c004d4
fromVeeko <77726018+fromVeeko@users.noreply.github.com>
2022-09-09 14:59:42 +0200

    scena5 bozza

--- ProjectEsame/scena5FineGioco.lua
+++ ProjectEsame/scena5FineGioco.lua
@@ -1,15 +1,34 @@
 -- Load the composer library 
 local composer = require( "composer" )
- 
--- define a new Scene 
-local scene = composer.newScene()
- 
 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
- 
+--variabili varie
+local scene = composer.newScene()
+
+local sfondo = display.newGroup()
+local pp = display.newGroup() 
+local background
+local mrb
+local dialogueBox
+local cloud
+
+local function createText(self, event)
+	if textN <= #textTable then
+		
+		dialogue:setFillColor(0,0,0)
+		dialogue.text = textTable[textN]
+		dialogue.anchorX = 0
+		dialogue.anchorY = 0
+		dialogue.x = display.contentCenterX/4
+		dialogue.y = display.contentHeight - 120
+		dialogue.font = fontDir
+		textN = textN + 1 --passa al testo successivo
+	else
+		audio.stop({channel=1})	
+
+		composer.removeScene("scena3_dungeon")
+		composer.gotoScene("scena3_dungeon", {effect = "zoomInOutFade",	time = 1000}) 
+	end
+end
 
  
  
@@ -19,8 +38,24 @@ local scene = composer.newScene()
  
 -- create()
 function scene:create( event )
- 
-   
+    local sceneGroup = self.view
+	textTable = {
+		"Mr B.: Complimenti, hai superato la prova",
+		"Mr B.: Ora puoi entrare ed esplorare il cuore della montagna",
+		"Mr B.: Evita tutti i nemici e trova la via d'uscita"
+	}
+
+    background = display.newImageRect(sfondo, "risorseGrafiche/PG/holyCrapAmIInHeaven.png", display.contentWidth, display.contentHeight)
+    mrb = display.newImageRect(pp, "risorseGrafiche/RaccoltaDiRoba/b.png", 300, 300)
+    cloud = display.newImageRect(pp,"risorseGrafiche/scenaIntro/nuvolaVoce.png", 400, 200)
+    dialogueBox = display.newImageRect(pp, "risorseGrafiche/boxmessaggi.png", display.contentWidth +100, display.contentHeight/1.5)
+	local fontDir = "risorseGrafiche/font/fontpixel.ttf"
+	local fontCustom = native.newFont(fontDir, 12)
+	dialogue = display.newText({text="",fontSize=25, font = fontDir})
+	sceneGroup:insert(sfondo)
+	sceneGroup:insert(pp)	  
+	sceneGroup:insert(mrb)
+	sceneGroup:insert(dialogue)
 end
 
 
@@ -33,11 +68,25 @@ function scene:show( event )
     local phase = event.phase
  
     if ( phase == "will" ) then
+		sfondo.x = display.contentCenterX
+		sfondo.y = display.contentCenterY - 100
+		sfondo:scale(0.3, 0.3)
+		textN = 0
+		mrb.x = display.contentCenterX
+		mrb.y = display.contentCenterY - 100
+		-- dialogue.anchorX = 0
+		-- dialogue.anchorY = 0
+		dialogue.x = display.contentCenterX
+		dialogue.y = display.contentCenterY
+		dialogueBox.x = display.contentCenterX
+		dialogueBox.y = display.contentHeight - dialogueBox.height/2 + 160
 		
 		
  
     elseif ( phase == "did" ) then
-      
+        createText(text)
+        -- activate the tap listener 
+		Runtime:addEventListener("tap", createText)
  
     end
 end
@@ -51,8 +100,7 @@ function scene:hide( event )
     local phase = event.phase
  
     if ( phase == "will" ) then
-        -- Code here runs when the scene is on screen (but is about to go off screen)
-		physics.pause()
+        Runtime:removeEventListener("tap", createText)
  
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
@@ -66,7 +114,7 @@ function scene:destroy( event )
  
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
- 
+    
 end
  
  
